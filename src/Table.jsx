0839933ad821720f01f4db6b34f0072595a65103
myselfrashid <rashidali.jmi@gmail.com>
2023-10-19 10:07:35 +0530

    ui changes

--- src/Table.jsx
+++ src/Table.jsx
@@ -28,7 +28,7 @@ const Table = ({ data }) => {
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{data.startDate}</td>
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{data.endDate}</td>
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{(new Date(data.startDate).getMonth() + 1)}</td>
-                                <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'><ul>{data.excludedDates.map((date, index) => (<p key={index}>{8}</p>))}</ul></td>
+                                <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'><ul>{data.excludedDates.map((date, index) => (<p key={index}>{date}</p>))}</ul></td>
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{days}</td>
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{data.leadCount}</td>
                                 <td className='lg:text-base md:text-xs xs:text-[7px] lg:w-32 md:w-20 xs:w-8 lg:font-normal xs:font-thin'>{Math.floor((data.leadCount) / (days))}</td>
